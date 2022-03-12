e7e7523834b3ae36f3a326b111b9f62dbadf990c
tngye <tngyeefong@gmail.com>
2022-03-13 02:07:47 +0800

    new

---
+++ src/main/java/sg/edu/nus/iss/WS11Redo/WS11Redo/Ws11RedoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.WS11Redo.WS11Redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws11RedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws11RedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/WS11Redo/WS11Redo/Ws11RedoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.WS11Redo.WS11Redo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws11RedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
